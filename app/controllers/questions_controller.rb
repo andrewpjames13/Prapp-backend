eb397cef6312c1d82a072abf1f6785cc16caf2a2
Andrew James <andrewpjames87@gmail.com>
2015-05-28 14:13:36 -0600

    Method update working with postman

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,25 @@
 class QuestionsController < ApplicationController
-
+  skip_before_filter  :verify_authenticity_token
   def index
     @questions = Question.all
     render json: { questions: @questions }.to_json
   end
 
   def show
-  @question = Question.find(params[:id])
-  render json: @question.to_json
+    @question = Question.find(params[:id])
+    render json: @question.to_json
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+    render nothing: true
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:correct)
   end
 
 end
